a4085c2c4093df6e95fc8787a244fdf5129ebc5b
Caio <caio.vr12@gmail.com>
2025-08-01 12:48:50 -0300

    organização básica de componentes

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import styles from '@/components/footer/Footer.module.css';
+
+export const Footer = () => {
+  return (
+    <footer className={styles.foot}>
+      <h2>Footer</h2>
+      <p>Footer here...</p>
+    </footer>
+  )
+}
---
+++ src/components/main/Main.tsx
@@ -0,0 +1,9 @@
+
+export const Main = () => {
+  return (
+    <main>
+      <h1>Main Block</h1>
+      <p>Main here</p>
+    </main>
+  )
+}
