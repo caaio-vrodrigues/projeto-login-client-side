280e0171e591ae3fcd8d0215564befce3adad08c
Caio <caio.vr12@gmail.com>
2025-08-04 10:35:07 -0300

    Trabalhando no design da página interna

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,21 @@
 import styles from '@/components/header/Header.module.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAppleAlt } from '@fortawesome/free-solid-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <h1>Header</h1>
-      <p>Header here</p>
+      <div className={`${styles.leftBlock} ${styles.block}`}>
+        <FontAwesomeIcon icon={faAppleAlt}/>
+      </div>
+      <div className={`${styles.centerBlock} ${styles.block}`}>
+        <h1>Dashboard</h1>
+      </div>
+      <div className={`${styles.rightBlock} ${styles.block}`}>
+        <FontAwesomeIcon icon={faBars}/>
+      </div>
     </header>
   )
 }
--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,7 +1,8 @@
+import styles from '@/components/main/Main.module.css';
 
 export const Main = () => {
   return (
-    <main>
+    <main className={styles.main}>
       <h1>Main Block</h1>
       <p>Main here</p>
     </main>
